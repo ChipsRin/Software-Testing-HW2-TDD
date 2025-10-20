60467999a9c441337c454581c17974c011d94f12
ChipsRin <yslan168@gmail.com>
2025-10-21 00:49:10 +0800

    RED: Add failing test for subtract

--- CalcTest.py
+++ CalcTest.py
@@ -7,5 +7,11 @@ class TestCalculator(unittest.TestCase):
         result = calc.add(2, 3)
         self.assertEqual(result, 5)  # Expect 2 + 3 = 5
 
+    # add a test for subtract method
+    def test_subtract(self):
+        calc = Calculator()
+        result = calc.subtract(10, 5)
+        self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+
 if __name__ == "__main__":
     unittest.main()
